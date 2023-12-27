e9a0720d715824b70fc47241a2a5fd21137b584e
Daniel <danieldrv8@gmail.com>
2023-12-27 10:22:56 +0100

    List wishes base

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,5 +1,11 @@
 import React from "react";
 
+const wishes = [
+    {text: 'Travel to the moon', done: false},
+    {text: 'Pay the gym', done: true},
+    {text: 'Going to the gym', done: false}
+]
+
 const App = () => (
     <div>
         <h1>My wishlist app</h1>
@@ -7,8 +13,16 @@ const App = () => (
             <legend>New wish</legend>
             <input placeholder="Enter your wish"/>
             <ul>
-                
+                {wishes.map(({text, done}, i) => (
+                <li>
+                    <label htmlFor={`wish${i}`}>
+                        <input id={`wish${i}`} type="checkbox" checked={done}></input>
+                        {text}
+                    </label>
+                    </li>
+                ))}
             </ul>
+            <button type="button" >Archive done</button>
         </fieldset>
     </div>
 )
