56efd8b58fb4ea69a64729891aa5ca4dba15d84d
Daniel <danieldrv8@gmail.com>
2023-12-27 10:10:19 +0100

    Proyecto base creado

---
+++ src/App/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const App = () => (
+    <div>
+        <h1>My wishlist app</h1>
+        <fieldset>
+            <legend>New wish</legend>
+            <input placeholder="Enter your wish"/>
+            <ul>
+                
+            </ul>
+        </fieldset>
+    </div>
+)
+
+export default App;
